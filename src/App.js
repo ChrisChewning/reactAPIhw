15b17e2403dcf892b1f1c401536590da8fa72a71
Chris <chewning.cl@gmail.com>
2018-07-31 21:55:16 -0500

    login page switches to new component on login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './Login';
+import MainContainer from './MainContainer';
+
+
 
 class App extends Component {
+  constructor() {
+    super();
+
+    //we want to keep track of both of these properties throughout our app so we put them in the main app.js file. we start off logged: false b.c when a user comes to the page they are not automatically logged in. same with username as an empty string.
+    this.state = {
+      logged: false,
+      username: ''
+    }
+  }
+
+
+//TO ADD FUNCTIONALITY TO LOGIN
+
+login = (username) => {
+  console.log('the login function in app is working', username);
+  this.setState({
+    username: username, //sets a variable to usernam?
+    logged: true //now you can toggle this.
+  })
+}
+
+
+
+
   render() {
+    console.log(this.state, ' inside of app component');
     return (
       <div className="App">
-        <h1>hi</h1>
-        <Login />
+        {/* <h1>hi</h1> */}
+
+
+         {/* if this.state.logged data is there(?) yes = show the MainContainer component. also username is now the logged in username in state. no = show the Login component. Also, login's variable will be set to this.login.  */}
+        {this.state.logged ? <MainContainer username={this.state.username} /> : <Login login={this.login} />}
+
       </div>
     );
   }
--- src/MainContainer/index.js
+++ src/MainContainer/index.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 
 class MainContainer extends Component {
+  //NOTE: you did not use constructor(){} or super() b.c you are not needing to do any actions. this is just a presentaional component(?) b.c you are taking in other components and presenting them to the client.
 
+  //NOTE 2: this.props.username NOT this.props.state. Where is the property started? App.js 15 or 25?
   
+  //NOTE: this.props.password won't show up.
+
+render() {
+  return(
+    <div>
+    <h1>hi</h1>
+    {this.props.username}
+  </div>
+  )
+}
 }
 
 
-export default Main Container;
+export default MainContainer;
